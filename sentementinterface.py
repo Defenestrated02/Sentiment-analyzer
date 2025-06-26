c4fd3c6c0fb359712c34eb0e7b13b69d27d5b8dd
Defenestrated02 <om.malkan@gmail.com>
2025-06-26 15:24:57 -0400

    Update sentementinterface.py

--- sentementinterface.py
+++ sentementinterface.py
@@ -6,7 +6,9 @@ from transformers import BertTokenizer, BertModel
 from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
 from transformers import BertTokenizer
+import gdown
 
+#making model
 class BERTClassifier(nn.Module):
     def __init__(self, bert_model_name, num_classes):
         super(BERTClassifier, self).__init__()
@@ -45,7 +47,6 @@ class BERTClassifier(nn.Module):
                 actual_labels.extend(labels.cpu().tolist())
         return accuracy_score(actual_labels, predictions), classification_report(actual_labels, predictions)
     def predict_sentiment(text, model, tokenizer, device, max_length=128):
-
         encoding = tokenizer(text, return_tensors='pt', max_length=max_length, padding='max_length', truncation=True)
         input_ids = encoding['input_ids'].to(device)
         attention_mask = encoding['attention_mask'].to(device)
@@ -55,12 +56,19 @@ class BERTClassifier(nn.Module):
         return "positive" if preds.item() == 1 else "negative"
     
 if __name__ == '__main__':
+    
+    #downloads pretrained model if it is not allready downloaded
+    if(not(os.path.isfile(os.getcwd() +"\\bert_classifier1.pth"))):
+        url = 'https://drive.google.com/file/d/1bN4eu00gT4RHsdPX8QZu81AVNBnyRp8u/view?usp=drive_link'
+        destination = os.getcwd() +"\\bert_classifier1.pth"
+        gdown.download(url, destination, quiet=False,fuzzy=True)
+        print(f"model dowloaded to {destination}")
     bert_model_name = 'bert-base-uncased'
     num_classes = 2
     tokenizer = BertTokenizer.from_pretrained(bert_model_name)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = BERTClassifier(bert_model_name, num_classes).to(device)
-    model.load_state_dict(torch.load('bert_classifier.pth', map_location=torch.device(device)))
-    #file = open(sys.argv[1]) 
-    text = "I hate every charater in this film"
+    model.load_state_dict(torch.load('bert_classifier1.pth', map_location=torch.device(device)))
+    file = open(sys.argv[1]) 
+    text = file.read()
     print(BERTClassifier.predict_sentiment(text, model, tokenizer, device, max_length=128))
